870f7bdc83d57256fe6d612c721854605f5e70aa
xok9ty <125658205+xok9ty@users.noreply.github.com>
2025-06-19 10:16:54 +0300

    create models for evaluation journal and register models on admin page

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from forum.models import Teacher, Student, Subject
 
 # Register your models here.
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(Subject)
