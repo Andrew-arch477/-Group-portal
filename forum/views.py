88e9cd310326e18a338095a4070e613953ee5512
dasha2020 <darenddhaker128675@gmail.com>
2025-06-19 14:41:38 +0300

    Add detailed forum page

--- forum/views.py
+++ forum/views.py
@@ -23,7 +23,10 @@ class DetailedForum(View):
         context["css_file"] = 'styles.css'
         return context
     
-    def get(self, request):
-        forums = Forum.objects.all()
-        context = self.get_context_data(forums=forums)
-        return render(request, 'forums.html', context)
+    def get(self, request, forum_id=None):
+        forum = Forum.objects.get(id=forum_id)
+        forums = Forum.objects.all().order_by('-created_date')
+        messages = forum.message_set.all().order_by('created_date')
+        context = self.get_context_data(forum=forum, messages=messages, forums=forums)
+        return render(request, 'detailed_forum.html', context)
+
