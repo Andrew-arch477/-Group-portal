bf775716fe551d995971fc2a9827be6c3240b899
dasha2020 <darenddhaker128675@gmail.com>
2025-06-20 10:22:41 +0300

    Add Message form

--- forum/forms.py
+++ forum/forms.py
@@ -4,3 +4,10 @@ from django.contrib.auth.models import User
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=100)
     password = forms.CharField(widget=forms.PasswordInput)
+
+class MessageForm(forms.Form):
+    text = forms.CharField(max_length=850,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Напиши щось...'
+        })
+)
