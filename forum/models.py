dd5c6a76d9483520069af90ace4cb7a537a99bae
Andrew-arch477 <forandrew1985@gmail.com>
2025-06-20 16:19:39 +0300

    Event model

--- forum/models.py
+++ forum/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import calendar
 
 # Create your models here.
 
@@ -12,6 +13,7 @@ class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
+
 
 class Teacher(models.Model):
     first_name = models.CharField(max_length=50)
@@ -28,8 +30,6 @@ class Teacher(models.Model):
         verbose_name_plural = "Teachers"
         unique_together = (("email"),)
 
-    
-
 class Subject(models.Model):
     subject_name = models.CharField(max_length=100)
     bio = models.TextField()
@@ -40,7 +40,6 @@ class Subject(models.Model):
     class Meta:
         verbose_name = "Subject"
         verbose_name_plural = "Subjects"
-
 
 class Student(models.Model):
     first_name = models.CharField(max_length=50)
@@ -58,3 +57,12 @@ class Student(models.Model):
         verbose_name = "Student"
         verbose_name_plural = "Students"
         unique_together = (("email"),)
+
+class Event(models.Model):
+    MONTH_CHOICES = [(i, calendar.month_name[i]) for i in range(1, 13)]
+
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    time = models.TimeField()
+    date = models.IntegerField()
+    mounth = models.CharField(choices=MONTH_CHOICES)
