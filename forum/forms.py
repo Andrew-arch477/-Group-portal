ee90a8fe5a5edb59b6a0a55495f0a42c3c3e277d
dasha2020 <darenddhaker128675@gmail.com>
2025-06-20 15:49:53 +0300

    Style calender, login

--- forum/forms.py
+++ forum/forms.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 import calendar
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=100)
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Type here...'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Type here...'}))
 
 class MessageForm(forms.Form):
     text = forms.CharField(max_length=850,
@@ -16,5 +16,5 @@ class MessageForm(forms.Form):
 class CalendarForm(forms.Form):
     MONTH_CHOICES = [(i, calendar.month_name[i]) for i in range(1, 13)]
     
-    month = forms.ChoiceField(choices=MONTH_CHOICES)
-    year = forms.IntegerField(min_value=1900, max_value=2100)
+    month = forms.ChoiceField(choices=MONTH_CHOICES, widget=forms.Select(attrs={'class': 'form-control',}))
+    year = forms.IntegerField(min_value=1900, max_value=2100, widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter year',}))
