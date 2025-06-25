d2580737fd96f2860abe36563b4c2b7189a9c5ca
Andrew-arch477 <forandrew1985@gmail.com>
2025-06-25 19:00:37 +0300

    Show event and filter it.

--- forum/admin.py
+++ forum/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from forum.models import Teacher, Student, Subject
+from forum.models import Teacher, Student, Subject, Event
 
 # Register your models here.
 admin.site.register(Teacher)
 admin.site.register(Student)
 admin.site.register(Subject)
+admin.site.register(Event)
