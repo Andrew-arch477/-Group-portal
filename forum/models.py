624b00c3624b1154c5585bec1b49a42349fa3521
vladultimate <vladultimate2010@gmail.com>
2025-06-26 04:30:29 +0300

    added a user role(user,admin,moderator)

--- forum/models.py
+++ forum/models.py
@@ -4,6 +4,15 @@ import calendar
 
 # Create your models here.
 
+class Profile(models.Model):
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('moderator', 'Moderator'),
+        ('user', 'User'),
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='user')
+
 class Forum(models.Model):
     title = models.CharField(max_length=200)
     created_date = models.DateTimeField(auto_now_add=True)
